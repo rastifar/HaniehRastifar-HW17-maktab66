e521e234e985e7504ad29d1e0fb610249db5da70
haniey <h.rastifar@gmail.com>
2022-04-06 11:14:15 -0700

    modification

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -1,18 +1,18 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 //Context
 import Checkbox from "./Checkbox";
 
 const AllItems = () => {
   // const { listItems } = useContext(checklistContext);
-  const [items, setItems] = useState([])
-  
-  useEffect(() => {  
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
     fetch("https://624b064444505084bc4ab819.mockapi.io/items")
       .then((response) => response.json())
       .then((json) => setItems(json))
-      .catch(error => {
-      alert('It seems to have problem loading data from API')
-    })
+      .catch((error) => {
+        alert("It seems to have problem loading data from API");
+      });
   }, []);
 
   return (
@@ -22,7 +22,7 @@ const AllItems = () => {
         {/* why does it print 4 times */}
         {/* {listItems.map(item=>(console.log('from allitems:',item)))} */}
         {items.map((item) => (
-          <Checkbox key={item.id} id={item.id} title={item.title} isChecked={item.checked}/>
+          <Checkbox key={item.id} id={item.id} title={item.title} />
         ))}
       </div>
     </div>
